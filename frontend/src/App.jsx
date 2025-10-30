eab2fda8f5da99eb399ac001505885f9e15fd207
abbywilson11 <awils141@uottawa.ca>
2025-10-30 03:02:08 -0400

    set up frontend for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header"; //import header
 import ResourcesSection from "./components/ResourcesSection"; // import resources
 import Footer from "./components/Footer"; //import footer
@@ -6,7 +6,13 @@ import Table from "./components/table"; //import footer
 import "./App.css"; //import app style
 
 export default function App() {
-  return (
+  useEffect(() => {
+    // in development, we'll connect to backend running on port 5000
+    fetch("http://localhost:5000/api")
+      .then((res) => res.json())
+      .catch((err) => console.error("Error fetching data:", err));
+   }, []);
+      return (
     <div>
       <Header />
       <Table />
