8f5b715b6b267da247a9a8895617ed194e21d108
abbywilson11 <awils141@uottawa.ca>
2025-10-30 14:15:01 -0400

    added updates to table.jsx

--- server.js
+++ server.js
@@ -3,27 +3,33 @@
 import express, { json } from 'express';
 import cors from 'cors'; //enables requests from frontend to backend
 import path from 'path'; //helps with file and directory paths
+import { fileURLToPath } from "url";
 
 const app = express(); // Create Express app instance
 const PORT = process.env.PORT || 5000;  // use Render's port (or 5000 locally)
+// Fix __dirname manually since ES modules don't have it
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors()); // Allow frontend to talk to backend
 app.use(express.json()); // Parse JSON bodies from requests
 
 // Example route to test
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from backend 👋' });
+  res.json({ message: 'Hello from backend 👋' }); //helped test backend connected to localhost
 });
 
 const __dirname1 = path.resolve();   // get absolute project path
-app.use(express.static(path.join(__dirname1, "../frontend/dist")));
+app.use(express.static(path.join(__dirname1, "../frontend/dist"))); //pull from dist for rendered frontend
 
 // for *any other* route (like /about, /resources), send the React index.html file
 app.get(/.*/, (req, res) => { // /.*/ matches all routes
-  res.sendFile(path.join(__dirname1, "../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname1, "../frontend/dist/index.html")); //pull index from dist
 });
 
+app.use(express.static(path.join(__dirname, '../frontend/public')));
+
 // Start server
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+app.listen(PORT, () => { //PORT helps connect frontend and backend
+  console.log(`✅ Server running on port ${PORT}`); //error handling 
 });
